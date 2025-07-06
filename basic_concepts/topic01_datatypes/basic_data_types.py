3056dffefaf83beff995212e0fc817c8cc284f47
maicol665 <maicolbeltran885@gmail.com>
2025-07-06 15:27:23 -0500

    asiganando tipo de datos variable

--- basic_concepts/topic01_datatypes/basic_data_types.py
+++ basic_concepts/topic01_datatypes/basic_data_types.py
@@ -1,4 +1,9 @@
 name = "michael"
 edad = 24
-birthday = False 
-money = 2000.53 
+birthday = False
+money = 2000.53
+
+name: str = "michael"
+edad: int = 24
+birthday: bool = False
+money: float = 2000.53
